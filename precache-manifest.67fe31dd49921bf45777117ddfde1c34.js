75069961203b1f3813594a4e5cc50132d21c9ec2
patricoferris <patrickferris17@gmail.com>
2018-11-30 18:24:08 +0000

    Updates

---
+++ precache-manifest.67fe31dd49921bf45777117ddfde1c34.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bbe9e198097596131906ee69bcde6a6",
+    "url": "/game-gig-one-button/static/media/logo.6bbe9e19.png"
+  },
+  {
+    "revision": "82ba7e9d4c5ac6acdc2527c00cca2451",
+    "url": "/game-gig-one-button/static/media/Museo_Slab_700.82ba7e9d.otf"
+  },
+  {
+    "revision": "d4b163b4f9b93898a1fa03b009921f27",
+    "url": "/game-gig-one-button/static/media/MuseoSlab-700.d4b163b4.woff"
+  },
+  {
+    "revision": "b858284461e7bcfbc18d0f58a25b9a84",
+    "url": "/game-gig-one-button/static/media/ESSG.b8582844.png"
+  },
+  {
+    "revision": "21d407f96dbf2714600f",
+    "url": "/game-gig-one-button/static/js/runtime~main.21d407f9.js"
+  },
+  {
+    "revision": "59ed6e82d10e867dfb79",
+    "url": "/game-gig-one-button/static/js/main.59ed6e82.chunk.js"
+  },
+  {
+    "revision": "58698c09cf58c7456918",
+    "url": "/game-gig-one-button/static/js/1.58698c09.chunk.js"
+  },
+  {
+    "revision": "59ed6e82d10e867dfb79",
+    "url": "/game-gig-one-button/static/css/main.20e79c96.chunk.css"
+  },
+  {
+    "revision": "6ec23b7e745caf57bd2253cce8749629",
+    "url": "/game-gig-one-button/index.html"
+  }
+];
