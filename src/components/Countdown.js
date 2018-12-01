18662913bfda14e63e8a6fbc7da4458284dff7b7
patricoferris <patrickferris17@gmail.com>
2018-12-01 08:34:43 +0000

    Update rules and fix clock

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-const endTime = new Date('December 1, 2018 22:30:00');
+const endTime = new Date('December 1, 2018 20:00:00');
 
 class Countdown extends Component {
   constructor() {
--- src/data/Rules.js
+++ src/data/Rules.js
@@ -4,7 +4,7 @@ const rules = {
   content: [
     "Also respect and look after the Intel Lab, FW11 and the Computer Lab. Note that food or drink may only be consumed in FW11, and we will be checking!",
     "You can work on your game in a team of up to four people.",
-    "Game-making commences at 10:30 and finishes at 20:30.",
+    "Game-making commences at 10:30 and finishes at 20:00.",
     "You are free to do whatever you like with your game after the Game Gig. You own the copyright to all the material you create during the event.",
     "You are free to use any tools or libraries available to you to create your game. You can start with any pre-existing code or content that you like and you are free to use third-party assets, as long as you let the judges know what you created yourself and what you didn't. Failure to do so could risk disqualification.",
     "It\'s your responsibility to make sure that you have the right to use third-party assets (for example, that they are public domain or available under an appropriate license).",
